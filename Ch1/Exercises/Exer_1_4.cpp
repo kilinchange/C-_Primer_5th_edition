7e23bc9fd5497c861fd84d0d67c118d390ea6543
KilinChange <819109619@qq.com>
2020-08-10 23:37:39 +0800

    2020/08/10: Finish part of Ch1.

---
+++ Ch1/Exercises/Exer_1_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int v1, v2;
+    std::cout << "Enter two numbers: " << std::endl;
+    std::cin >> v1 >> v2;
+    std:: cout << "Product of the two numbers is: "
+               << v1 * v2 << std::endl;
+
+    return 0;
+}
