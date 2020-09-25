7632f9923b4d97b7145834adf08e4016a813b2a6
KilinChange <819109619@qq.com>
2020-09-25 20:36:20 +0800

    2020/09/25: Read to 7.1.3~

---
+++ Ch7/7.1.2/7.4-5.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+struct Person {
+    std::string get_name() const { return name; }
+    std::string get_address() const { return address; }
+
+    std::string name;
+    std::string address;
+};
